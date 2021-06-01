ee443609ad0b25c0262b9e40a8b21e084876ff99
zq-alt <73477878+zq-alt@users.noreply.github.com>
2021-06-01 10:40:06 +0800

    Update shell.bash

--- shell.bash
+++ shell.bash
@@ -2,3 +2,23 @@
 
 ################# linux shell practice #################
 
+#Linux下查询文件是否存在，若存在，写出文件存在的目录
+#!/bin/bash
+read -p "输入您要查询的文件名：" file   #read 写入变量
+if [ `find / -name $file|wc -l` -ne 0 ];then  #判断文件$files是否存在
+   find / -name $file > file.txt       #若存在，将查找到的内容写入到file.txt
+   if [ `cat file.txt|wc -l` -eq 1 ];then  
+      echo "$file只存在一个"
+      echo "文件存在的目录为： `sed 's/'$file'//g' file.txt`"
+   else
+     echo "$file存在多个目录"
+     while read line
+     do
+     echo "文件存在的目录为：${line%/*}"
+     done < file.txt
+   fi
+else
+   echo "$file不存在"
+fi
+
+
